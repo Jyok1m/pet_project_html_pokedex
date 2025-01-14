0093678443791ad3a3b66831570fd0efbb1098d2
Joachim Alexandre Jasmin <joachim.jasmin@gmail.com>
2025-01-14 11:29:35 +0100

    feat: func to generate pokemon card

---
+++ script.js
@@ -0,0 +1,19 @@
+// Purpose: generate pokemon card structure
+const generatePokemonCard = ({ id, name, primaryType }) => {
+	// Format data
+	id = parseInt(id);
+	name = name[0].toUpperCase() + name.slice(1).toLowerCase();
+	primaryType = primaryType.toLowerCase();
+
+	return `
+    <div class="pokemon ${primaryType}">
+      <div class="imgContainer">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png" alt="${name}" />
+      </div>
+      <div class="info">
+        <h3 class="name">${name}</h3>
+        <span class="type">Type: <span>${primaryType}</span></span>
+      </div>
+    </div>
+  `;
+};
